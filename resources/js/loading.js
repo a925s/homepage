2775b9fd474c8f9784fbab3105f9cc331e45bb53
a925s <a_925s@yahoo.co.jp>
2022-02-01 11:41:48 +0900

    編集

--- resources/js/loading.js
+++ resources/js/loading.js
@@ -1,15 +1,14 @@
 $(function() {
     $('.container-fluid').css('display','none');
-    var h = $(window).height();
+    $('.loading, .loading-5').css('display','block');
+});
     
-    $('.loading, .loading-5').height(h).css('display','block');
-    
-    $(window).on('load', function () { //全ての読み込みが完了したら実行
-        $('.loading').delay(900).fadeOut(800);
-        $('.loading-5').delay(600).fadeOut(300);
-        $('.container-fluid').delay(1700).queue(function(){
-            $(this).css('display', 'block');
-        });
+$(window).on('load', function () { //全ての読み込みが完了したら実行
+    $('.loading').delay(900).fadeOut(800);
+    $('.loading-5').delay(600).fadeOut(300);
+    $('.container-fluid').delay(1700).queue(function(){
+        $(this).css('display', 'block');
     });
 });
+
 
