4630454e547acf205f2c98d710cd59616ba91143
a925s <a_925s@yahoo.co.jp>
2022-02-01 11:36:37 +0900

    js

--- public/js/loading.js
+++ public/js/loading.js
@@ -10987,16 +10987,16 @@ return jQuery;
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function($) {$(function () {
-  var h = $(window).height();
   $('.container-fluid').css('display', 'none');
+  var h = $(window).height();
   $('.loading, .loading-5').height(h).css('display', 'block');
-});
-$(window).on('load', function () {
-  //全ての読み込みが完了したら実行
-  $('.loading').delay(900).fadeOut(800);
-  $('.loading-5').delay(600).fadeOut(300);
-  $('.container-fluid').delay(1700).queue(function () {
-    $(this).css('display', 'block');
+  $(window).on('load', function () {
+    //全ての読み込みが完了したら実行
+    $('.loading').delay(900).fadeOut(800);
+    $('.loading-5').delay(600).fadeOut(300);
+    $('.container-fluid').delay(1700).queue(function () {
+      $(this).css('display', 'block');
+    });
   });
 });
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
--- resources/js/loading.js
+++ resources/js/loading.js
@@ -1,15 +1,15 @@
 $(function() {
+    $('.container-fluid').css('display','none');
     var h = $(window).height();
     
-    $('.container-fluid').css('display','none');
     $('.loading, .loading-5').height(h).css('display','block');
-});
     
-  $(window).on('load', function () { //全ての読み込みが完了したら実行
-    $('.loading').delay(900).fadeOut(800);
-    $('.loading-5').delay(600).fadeOut(300);
-    $('.container-fluid').delay(1700).queue(function(){
-        $(this).css('display', 'block');
+    $(window).on('load', function () { //全ての読み込みが完了したら実行
+        $('.loading').delay(900).fadeOut(800);
+        $('.loading-5').delay(600).fadeOut(300);
+        $('.container-fluid').delay(1700).queue(function(){
+            $(this).css('display', 'block');
+        });
     });
 });
 
